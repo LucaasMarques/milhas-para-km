43ef3ad84dacd80c5577d30d126cab98c378ac74
lucas <lucsalmeida004@gmail.com>
2025-09-26 22:35:37 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("milhas para km");      // saida
+Console.Write("informe o valor em milhas:");
+string entrada = Console.ReadLine()!;
+double milhas = Convert.ToDouble (entrada);
+double resultado = milhas * 1.609;
+Console.WriteLine($"{milhas} milhas em km --> {resultado}");
+
+
+
+
+
